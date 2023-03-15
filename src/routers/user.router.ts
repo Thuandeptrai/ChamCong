0f434777deda7d55f4fe4e75c774542a61f8d00b
Thuandeptrai <tvc4567@gmail.com>
2023-03-15 15:11:43 +0700

    ZX

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -8,7 +8,7 @@ router.get('/init', authenticate, checkValidToken)
 // dang ky  cho admin
 router.get('/initForAdmin', authenticateforAdmin, checkValidToken)
 
-router.post('/register', signUp)
+router.post('/register',authenticateforAdmin, signUp)
 // update
 router.post('/:userId',authenticateforAdmin, updateUser)
 // get alll user
