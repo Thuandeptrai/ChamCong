622ed3326d159bc9006214a544acea1d4a72a222
Thuandeptrai <tvc4567@gmail.com>
2023-03-20 16:08:35 +0700

    asdasd

--- src/controllers/workday.controller.ts
+++ src/controllers/workday.controller.ts
@@ -12,13 +12,17 @@ export const getWorkDayByUserId = async (
 ) => {
   const UserId = req.params.userId;
   try {
-    const getAllWorkDay = await ticketForUser.find({ userId: UserId });
-    const getWorkday = await  workRecordForUser.findOne({dateWork:getAllWorkDay})
-    console.log(getAllWorkDay)
+    const getAllWorkDay = await ticketForUser.find({ userId: UserId }).sort({DateIn: 'descending'});
+    const s = '3'
+      const regex = new RegExp(s, 'i') //
+    const getWorkday : any = await  workRecordForUser.find({ "month": { "$regex": regex }, userId: UserId})
+    console.log(getWorkday)
    const  response = responseModel(
       RESPONSE_STATUS.SUCCESS,
       'Get WorkDay User Success',
-      getAllWorkDay
+      getAllWorkDay,
+      undefined,
+      getWorkday
     );
     return res.status(200).json(response);
   } catch (err) {
